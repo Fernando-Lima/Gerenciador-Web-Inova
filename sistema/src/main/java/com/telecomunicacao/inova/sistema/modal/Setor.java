6b0b8675edd23cd53c265f0dc2dc1e65168917bf
Fernando-Lima <fernandosupweb@gmail.com>
2018-05-22 12:00:12 -0300

    Implementado classes Modal

---
+++ sistema/src/main/java/com/telecomunicacao/inova/sistema/modal/Setor.java
@@ -0,0 +1,38 @@
+package com.telecomunicacao.inova.sistema.modal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Setor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigo;
+	private String nome;
+	@ManyToOne
+	@JoinColumn(name="unidade_IdUnidade")
+	private Unidade unidade;
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Unidade getUnidade() {
+		return unidade;
+	}
+	public void setUnidade(Unidade unidade) {
+		this.unidade = unidade;
+	}
+}
