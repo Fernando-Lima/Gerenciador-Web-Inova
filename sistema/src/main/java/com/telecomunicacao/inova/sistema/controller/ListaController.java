fda9df3776b5e8fcb0278c64508d913872ce20da
Fernando-Lima <fernandosupweb@gmail.com>
2018-05-22 12:01:42 -0300

    adicionado metodo teste para salvar dados da lista de ramais

--- sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/ListaController.java
+++ sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/ListaController.java
@@ -2,6 +2,9 @@ package com.telecomunicacao.inova.sistema.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.telecomunicacao.inova.sistema.modal.Lista;
 
 @Controller
 @RequestMapping("/admin/lista")
@@ -14,5 +17,10 @@ public class ListaController {
 	@RequestMapping("/novo")
 	public String novo() {
 		return "CadastroLista";
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Lista lista) {
+		System.out.println(">>>> " + lista.getResponsavel()+" "+ lista.getRamal()+" "+lista.getTelefone());
+		return "Lista";
 	}
 }
