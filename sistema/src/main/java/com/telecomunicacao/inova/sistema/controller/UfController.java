8c7270df8f9fa7dad95656ef93065109d2c01c3b
Fernando-Lima <fernandosupweb@gmail.com>
2018-06-29 14:49:38 -0300

    Retirado metodos DAO da classe controller e passado para a classe de serviço

--- sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/UfController.java
+++ sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/UfController.java
@@ -34,16 +34,10 @@ public class UfController {
 	public ModelAndView listar() {
 		ModelAndView mv;
 		try {
-			// metogo get retorna um List com lista de obejo
-			ResponseEntity<List<Uf>> estados = conectaWS.getRestTemplate().exchange("/uf",HttpMethod.GET, null,  
-					new ParameterizedTypeReference<List<Uf>>() {
-			});
+			List<Uf> estados = conectaWS.listAll();
 			mv = new ModelAndView("PesquisaEstados");
-			mv.addObject("estados", estados.getBody());
+			mv.addObject("estados", estados);
 		} catch (Exception e) {
-//			Message msg = new Message(404);
-//			String message = msg.getMessage();
-//			mv.addObject("mensagem", "Servidor"+message);
 			mv = new ModelAndView("404");
 		}
 		return mv;
@@ -52,10 +46,9 @@ public class UfController {
 	//Buscar estado pelo codigo
 	@RequestMapping("{codigo}")
 	public ModelAndView buscarPorId(@PathVariable("codigo") Long codigo) {
-			//metodo get retorna um objeto pelo codigo 
-			ModelAndView mv = new ModelAndView("PesquisaEstados");
-			Uf uf = conectaWS.getRestTemplate().getForObject("/uf/{id}", Uf.class, codigo);
-			mv.addObject("estados", uf);
+			ModelAndView mv = new ModelAndView("cadastroUf");
+			Uf uf = conectaWS.findById(codigo);
+			mv.addObject(uf);
 		return mv;
 	}
 	
@@ -70,13 +63,30 @@ public class UfController {
 	//Salvando o objeto
 	@RequestMapping(value="/salvar")
 	public String salvar(Uf uf, RedirectAttributes attributes) {
-		System.out.println(uf);
 		try {
-			ResponseEntity<Uf> ufPost = conectaWS.getRestTemplate().exchange("/uf",HttpMethod.POST, new HttpEntity<>(uf,conectaWS.createJSONHeader()), Uf.class);
-			attributes.addFlashAttribute("mensagem","Estado salvo com sucesso");
+			if(uf.getCodigo()== null) {
+				conectaWS.save(uf);
+				attributes.addFlashAttribute("mensagem","Estado "+uf.getNome() +" salvo com sucesso!");
+			}
+			else {
+				conectaWS.update(uf);
+				attributes.addFlashAttribute("mensagem","Estado "+uf.getNome() +" atualizado com sucesso!");
+			}
 			return "redirect:/estados/novo";
 		} catch (Exception e) {
+			e.printStackTrace();
 			return "404";
 		}
+	}
+	
+	@RequestMapping(value="{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo, RedirectAttributes attributes) {
+		try {
+			conectaWS.delete(codigo);
+			attributes.addFlashAttribute("mensagem","Estado excluído com sucesso");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "redirect:/estados";
 	}
 }
