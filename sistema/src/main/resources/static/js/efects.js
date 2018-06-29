bcf72252d7dcd67cf88382a2d5d1708b35c3df2a
Fernando-Lima <fernandosupweb@gmail.com>
2018-06-29 15:55:23 -0300

    Java script para passar o codigo na url para exclusão

--- sistema/src/main/resources/static/js/efects.js
+++ sistema/src/main/resources/static/js/efects.js
@@ -4,3 +4,18 @@ $(document).ready(function() {
         $(".alert").alert('close');
     }, 5000);
 });
+
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event){
+	var button = $(event.relatedTarget);
+	var codigo = button.data('codigo');
+	var descricao = button.data('descricao');
+	
+	var modal = $(this);
+	var form = modal.find('form');
+	var action = form.data('url-base');
+	if(!action.endsWith('/')){
+		action += '/';
+	}
+	form.attr('action', action + codigo);
+	modal.find('.modal-body span').html('Tem certeza de deseja excluir <strong>' + descricao + '</strong> ?');
+});
