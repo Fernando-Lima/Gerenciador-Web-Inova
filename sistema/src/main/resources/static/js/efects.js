7fbfd98ece3939c7e26c6c4f54214ebc18237ee1
Fernando-Lima <fernandosupweb@gmail.com>
2018-07-02 16:57:30 -0300

    Implementação de multi level menu sidebar com JQuery

--- sistema/src/main/resources/static/js/efects.js
+++ sistema/src/main/resources/static/js/efects.js
@@ -19,3 +19,29 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event){
 	form.attr('action', action + codigo);
 	modal.find('.modal-body span').html('Tem certeza de deseja excluir <strong>' + descricao + '</strong> ?');
 });
+
+
+(function($){
+	$(document).ready(function(){
+
+	$('#cssmenu li.active').addClass('open').children('ul').show();
+		$('#cssmenu li.has-sub>a').on('click', function(){
+			$(this).removeAttr('href');
+			var element = $(this).parent('li');
+			if (element.hasClass('open')) {
+				element.removeClass('open');
+				element.find('li').removeClass('open');
+				element.find('ul').slideUp(200);
+			}
+			else {
+				element.addClass('open');
+				element.children('ul').slideDown(200);
+				element.siblings('li').children('ul').slideUp(200);
+				element.siblings('li').removeClass('open');
+				element.siblings('li').find('li').removeClass('open');
+				element.siblings('li').find('ul').slideUp(200);
+			}
+		});
+
+	});
+})(jQuery);
