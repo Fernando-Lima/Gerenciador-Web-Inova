d79b313bff220e3e4b87690dc062fae8cbc5c8ce
Fernando-Lima <fernandosupweb@gmail.com>
2018-07-02 09:52:12 -0300

    Implementado metodo listar

--- sistema/src/main/java/com/telecomunicacao/inova/sistema/service/UfDAO.java
+++ sistema/src/main/java/com/telecomunicacao/inova/sistema/service/UfDAO.java
@@ -2,10 +2,26 @@ package com.telecomunicacao.inova.sistema.service;
 
 
 
+import java.util.List;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.telecomunicacao.inova.sistema.modal.MinhaEntidade;
+import com.telecomunicacao.inova.sistema.modal.Uf;
 @Service
 public class UfDAO<T extends MinhaEntidade> extends RestTemplateDAO<T>{
+	private RestTemplate restTemplate = new RestTemplateBuilder().rootUri("https://aw-inova.cfapps.io").build();
 
+	public List<Uf> listAll(){
+		ResponseEntity<List<Uf>> entity = restTemplate.exchange("/uf", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Uf>>() {
+				});
+		return entity.getBody();
+	}
+	
 }
