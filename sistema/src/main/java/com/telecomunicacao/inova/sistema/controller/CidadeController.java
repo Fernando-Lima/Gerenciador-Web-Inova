8bedbfd3c96a8af34115e40434488f23842a920c
Fernando-Lima <fernandosupweb@gmail.com>
2018-07-04 11:09:21 -0300

    implementado combo dinâmico

--- sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/CidadeController.java
+++ sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/CidadeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.telecomunicacao.inova.sistema.modal.Cidade;
 import com.telecomunicacao.inova.sistema.modal.Uf;
 import com.telecomunicacao.inova.sistema.service.CidadeDAO;
+import com.telecomunicacao.inova.sistema.service.UfDAO;
 
 @Controller
 @RequestMapping("/cidades")
@@ -23,13 +25,16 @@ public class CidadeController {
 	@Autowired
 	CidadeDAO<Cidade> dao;
 	
+	@Autowired
+	UfDAO<Uf> ufDao;
+	
 	@RequestMapping
 	public ModelAndView listar() {
 		ModelAndView mv;
 		try {
 			List<Cidade> cidades = dao.listAll();
 			mv  = new ModelAndView("PesquisaCidades");
-			mv.addObject("cidades", cidades);
+			mv.addObject("cidade", cidades);
 		} catch (Exception e) {
 			mv = new ModelAndView("404");
 		}
@@ -82,5 +87,12 @@ public class CidadeController {
 				e.printStackTrace();
 			}
 			return "redirect:/cidades";
+		}
+		
+		//metodo para montar o combo dinamicamente
+		@ModelAttribute("listaUf")
+		public List<Uf> listaUf(){
+			List<Uf> estados = ufDao.listAll();
+			return estados;
 		}
 }
