f7d3f78f4595989ba2995249eaaadd1881b46dfa
Fernando-Lima <fernandosupweb@gmail.com>
2018-10-22 00:02:07 -0200

    Retornado as atividades, tela de cadastro de patrimonios

--- sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/PatrimonioController.java
+++ sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/PatrimonioController.java
@@ -1,14 +1,33 @@
 package com.telecomunicacao.inova.sistema.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.telecomunicacao.inova.sistema.modal.Patrimonio;
+import com.telecomunicacao.inova.sistema.service.PatrimonioDAO;
 
 @Controller
 @RequestMapping("/patrimonios")
 public class PatrimonioController {
-
 	
-	public String novo() {
-		return "tabelaPatrimonio";
+	@Autowired
+	PatrimonioDAO<Patrimonio> dao; 
+
+	@RequestMapping
+	public ModelAndView novo() {
+		ModelAndView mv;
+		try {
+			List<Patrimonio> todosPatrimonios = dao.listAll();
+			mv = new ModelAndView("PesquisaPatrimonios");
+			mv.addObject("patrimonios", todosPatrimonios);
+		} catch (Exception e) {
+			e.printStackTrace();
+			mv = new ModelAndView("404");
+		}
+		return mv;
 	}
 }
