6b0e485e5230d742a4d06bb04480d7d9ffd0259d
Fernando-Lima <fernandosupweb@gmail.com>
2018-11-10 22:45:46 -0200

    Retirado anotação de entidade

--- sistema/src/main/java/com/telecomunicacao/inova/sistema/modal/Setor.java
+++ sistema/src/main/java/com/telecomunicacao/inova/sistema/modal/Setor.java
@@ -1,13 +1,8 @@
 package com.telecomunicacao.inova.sistema.modal;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-
-@Entity
 public class Setor extends MinhaEntidade{
 
 	private String nome;
-	@ManyToOne
 	private Empresa empresa;
 	
 	public String getNome() {
