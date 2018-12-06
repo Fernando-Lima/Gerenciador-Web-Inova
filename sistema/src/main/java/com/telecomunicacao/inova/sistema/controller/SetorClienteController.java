090a11c7198f43f4138bb451044862b8d25f9b43
Fernando-Lima <fernandosupweb@gmail.com>
2018-12-06 17:13:50 -0200

    implementando metodos básicos do controller de setorCliente

--- sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/SetorClienteController.java
+++ sistema/src/main/java/com/telecomunicacao/inova/sistema/controller/SetorClienteController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.telecomunicacao.inova.sistema.modal.SetorCliente;
 import com.telecomunicacao.inova.sistema.service.SetorClienteDAO;
@@ -30,4 +32,52 @@ public class SetorClienteController {
 		}
 		return mv;
 	}
+	//novo
+	@RequestMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("cadastroSetorCliente");
+		mv.addObject(new SetorCliente());
+		return mv;
+	}
+	
+	//buscarPorId
+	@RequestMapping("{codigo}")
+	public ModelAndView findById(@PathVariable Long codigo) {
+		ModelAndView mv = new ModelAndView("cadastroSetorCliente");
+		SetorCliente setorCliente = setorClienteDao.buscar(SetorCliente.class, codigo, TAG);
+		mv.addObject(setorCliente);
+		return mv;
+	}
+	
+	//salvar
+	@RequestMapping(value="/salvar")
+	public String salvar(SetorCliente setorCliente, RedirectAttributes attributes) {
+		try {
+			if(setorCliente.getCodigo() == null) {
+				setorClienteDao.salvar(SetorCliente.class, setorCliente, TAG);
+				attributes.addFlashAttribute("mensagem", "Setor " + setorCliente.getNome() + " salvo com sucesso!");
+			}else {
+				setorClienteDao.atualizar(SetorCliente.class, setorCliente, TAG);
+				attributes.addFlashAttribute("mensagem", "Setor " + setorCliente.getNome() + " atualizado com sucesso!");
+			}
+			return "redirect:/setorClientes/novo";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "404";
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
